57de66d38cfb3db2a12daaa7ce731f43bda41def
Karol Krasnowski <karolkrasnowski@outlook.com>
2021-11-06 11:05:10 +0100

    Title and subtitle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
   <v-container class="main-container">
+    <div class="text-h3 mb-5 mt-5 font-weight-medium" align="center">
+      ByteCrypt
+    </div>
+    <div
+      class="text-h6 grey--text mb-5 font-weight-light font-italic"
+      align="center"
+    >
+      Encrypt your files without leaving your browser
+    </div>
     <stepper />
   </v-container>
 </template>
